32278f4c0340648ad9a400cf9689e7690cce235e
Jatin Gupta <jatingupta108@gmail.com>
2025-03-20 23:39:25 +0530

    uvicorn port changed

--- backend/App/main.py
+++ backend/App/main.py
@@ -95,7 +95,7 @@ async def process(file: UploadFile = File(...), query: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app , host = '0.0.0.0')
 
 
 
